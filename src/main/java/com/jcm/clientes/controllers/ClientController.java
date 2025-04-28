49b616a36f305120d471c9c11f0918c4780bb312
Júlio Cesar Melegari <juliocesar.melegari@gmail.com>
2025-04-28 14:15:33 -0300

    add methods insert, update, delete

--- src/main/java/com/jcm/clientes/controllers/ClientController.java
+++ src/main/java/com/jcm/clientes/controllers/ClientController.java
@@ -1,16 +1,25 @@
 package com.jcm.clientes.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jcm.clientes.dtos.ClientDto;
 import com.jcm.clientes.services.ClientService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -29,6 +38,26 @@ public class ClientController {
     public ResponseEntity<ClientDto> findById(@PathVariable("id") Long id){
     	ClientDto dto =  clientService.findById(id);
     	return ResponseEntity.ok(dto);
+    }
+    
+    @PostMapping
+    public ResponseEntity<ClientDto> insert(@Valid @RequestBody ClientDto dto){
+    	dto = clientService.insert(dto);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+    				.buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+    
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDto> update(@PathVariable("id") Long id, @Valid @RequestBody ClientDto dto){
+    	dto =  clientService.update(id,dto);
+    	return ResponseEntity.ok(dto);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+    	clientService.delete(id);
+    	return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/jcm/clientes/services/ClientService.java
+++ src/main/java/com/jcm/clientes/services/ClientService.java
@@ -1,16 +1,20 @@
 package com.jcm.clientes.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jcm.clientes.dtos.ClientDto;
 import com.jcm.clientes.entities.Client;
 import com.jcm.clientes.repositories.ClientRepository;
 import com.jcm.clientes.services.exceptions.ResourceNotFoundException;
+import com.jcm.clientes.services.exceptions.DatabaseException;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -31,4 +35,44 @@ public class ClientService {
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		return new ClientDto(client);
 	}
+	
+	@Transactional
+	public ClientDto insert(ClientDto dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = clientRepository.save(entity);
+		return new ClientDto(entity);
+	}
+	
+	@Transactional
+	public ClientDto update(Long id, ClientDto dto) {
+		try {
+			Client entity = clientRepository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = clientRepository.save(entity);
+			return new ClientDto(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!clientRepository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+			clientRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
+		}
+	}
+	
+	private void copyDtoToEntity(ClientDto dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}	
 }
